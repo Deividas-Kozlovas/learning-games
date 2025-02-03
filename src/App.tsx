40aab0349f9b3027f7c0607b3d3240785aaf8d80
Deividas Kozlovas <kozlovas.deividas@gmail.com>
2025-02-03 21:49:27 +0200

    reainstaled all project to use vite with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/home/HomePage.tsx";
-import FindNumbersUpToTen from "./pages/games/numberGames/FindNumbersUpToTen/FindNumbersUpToTen.tsx";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route
-          path="/find-numbers-up-to-ten"
-          element={<FindNumbersUpToTen />}
-        />
-      </Routes>
-    </Router>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/moduls/utils/randomColorGeneratorUtils.tsx
+++
@@ -1,5 +0,0 @@
-export const getRandomLightColor = () => {
-  return `rgb(${Math.random() * 128 + 128}, ${Math.random() * 128 + 128}, ${
-    Math.random() * 128 + 128
-  })`;
-};
