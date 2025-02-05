bed1d03e2cd1a2bb9ffee42f3218b0173b743309
Deividas Kozlovas <kozlovas.deividas@gmail.com>
2025-02-05 12:58:48 +0200

    refactor: add images to html from scss and add alt description to improve seo

--- src/pages/games/numberGames/findNumberByWord/FindNumberByWord.tsx
+++ src/pages/games/numberGames/findNumberByWord/FindNumberByWord.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { useCardGameContext } from "../../../../context/CardGameContext"; // Use the custom hook to access the context
+import { useCardGameContext } from "../../../../context/CardGameContext";
 import CardComponents from "../../../../components/gameComponents/cardComponent/CardComponent";
 import FeedbackMessageComponent from "../../../../components/gameComponents/feedbackMessageComponent/FeedbackMessageComponent";
 import ProgressComponent from "../../../../components/gameComponents/progressComponent/ProgressComponent";
@@ -8,6 +8,7 @@ import GameOverComponent from "../../../../components/gameComponents/gameOverCom
 import { numbersToFindInWords } from "./findNumberByWordData";
 import "./findNumberByWordStyles.scss";
 import { numbersToShowOnCards } from "../../../../pages/games/numberGames/findNumberByWord/findNumberByWordData";
+import backgroundImage from "../../../../assets/images/background/findNumberByWordBackground.jpg";
 
 function FindItemByWord() {
   const { state, startGame, handleCardClick } = useCardGameContext();
@@ -18,26 +19,33 @@ function FindItemByWord() {
 
   return (
     <div className="find-numbers-game-background">
-      <ProgressComponent progress={state.currentItemIndex * 10} />
-      <Container>
-        <Row>
-          <Col
-            className="card"
-            style={{ backgroundColor: "rgb(151, 212, 159)" }}
-          >
-            <p className="card__text">
-              {numbersToFindInWords[state.currentItemIndex]}
-            </p>
-          </Col>
-        </Row>
-        <CardComponents
-          chunkedArray={state.chunkedItems}
-          cardsBackgroundColors={state.itemsBackgroundColors}
-          handleCardClick={handleCardClick}
-        />
-      </Container>
-      <FeedbackMessageComponent isCorrect={state.isAnswerCorrect} />
-      {state.gameOver === true && <GameOverComponent />}
+      <img
+        src={backgroundImage}
+        alt="Žaidimas, kuriame reikia rasti skaičius nuo vieno iki dešimties pagal žodžius"
+        className="find-numbers-game-background-image"
+      />
+      <div className="find-numbers-game-content">
+        <ProgressComponent progress={state.currentItemIndex * 10} />
+        <Container>
+          <Row>
+            <Col
+              className="card"
+              style={{ backgroundColor: "rgb(151, 212, 159)" }}
+            >
+              <p className="card__text">
+                {numbersToFindInWords[state.currentItemIndex]}
+              </p>
+            </Col>
+          </Row>
+          <CardComponents
+            chunkedArray={state.chunkedItems}
+            cardsBackgroundColors={state.itemsBackgroundColors}
+            handleCardClick={handleCardClick}
+          />
+        </Container>
+        <FeedbackMessageComponent isCorrect={state.isAnswerCorrect} />
+        {state.gameOver === true && <GameOverComponent />}
+      </div>
     </div>
   );
 }
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import "./homePageStyles.scss";
+import numbersButton from "../../assets/images/buttons/numbers-button.jpeg";
+import lettersButton from "../../assets/images/buttons/letters-button.jpeg";
+import colorsButton from "../../assets/images/buttons/colors-button.jpeg";
+import wordsButton from "../../assets/images/buttons/words-button.jpeg";
+import confettiImage from "../../assets/images/confetti-doodles.svg";
 
 function HomePage() {
   return (
@@ -13,6 +18,11 @@ function HomePage() {
                 className="select-game__text select-game__text--numbers-games"
                 variant="primary"
               >
+                <img
+                  src={numbersButton}
+                  alt="Žaidimai su skaičiais, lavinantys matematinius įgūdžius"
+                  className="select-game__image"
+                />
                 Žaidimai su skaičiais
               </Button>
             </Link>
@@ -23,6 +33,11 @@ function HomePage() {
                 className="select-game__text select-game__text--letters-games"
                 variant="primary"
               >
+                <img
+                  src={lettersButton}
+                  alt="Žaidimai su žodžiais, lavinantys žodyną ir kalbos įgūdžius"
+                  className="select-game__image"
+                />
                 Žaidimai su raidėm
               </Button>
             </Link>
@@ -35,6 +50,11 @@ function HomePage() {
                 className="select-game__text select-game__text--colors-games"
                 variant="primary"
               >
+                <img
+                  src={colorsButton}
+                  alt="Žaidimai su spalvom"
+                  className="select-game__image"
+                />
                 Žaidimai su spalvom
               </Button>
             </Link>
@@ -45,12 +65,25 @@ function HomePage() {
                 className="select-game__text select-game__text--word-games"
                 variant="primary"
               >
+                <img
+                  src={wordsButton}
+                  alt="Žaidimai su žodžiais"
+                  className="select-game__image"
+                />
                 Žaidimai su žodžiais
               </Button>
             </Link>
           </Col>
         </Row>
       </Container>
+
+      <div className="select-game__background">
+        <img
+          src={confettiImage}
+          alt="Žaidimai lavinimui ir smegenų treniravimui"
+          className="select-game__background-image"
+        />
+      </div>
     </div>
   );
 }
