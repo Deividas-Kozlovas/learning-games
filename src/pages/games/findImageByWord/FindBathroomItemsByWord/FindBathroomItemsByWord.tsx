b713a3c77cad1a0cdc07d323e41d795bae0d8e8b
Deividas-Kozlovas <kozlovas.deividas@gmail.com>
2025-02-16 20:01:23 +0200

    add: game to find images of bathroom items by word

---
+++ src/pages/games/findImageByWord/FindBathroomItemsByWord/FindBathroomItemsByWord.tsx
@@ -0,0 +1,65 @@
+import { useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+import ImageCardComponents from "../../../../components/gameComponents/ImageCardComponent/ImageCardComponent";
+import FeedbackMessageComponent from "../../../../components/gameComponents/feedbackMessageComponent/FeedbackMessageComponent";
+import ProgressComponent from "../../../../components/gameComponents/progressComponent/ProgressComponent";
+import GameOverComponent from "../../../../components/gameComponents/gameOverComponent/GameOverComponent";
+
+import { bathroomItems } from "./FindBathroomItemsByWordData";
+import { useCardGameContext } from "../../../../context/CardGameContext";
+import sound from "../../../../assets/images/icons/sound.svg";
+import talkingSound from "../../../../assets/images/icons/talking-sound.svg";
+import { SET_DISPLAY_TEXT } from "../../../../actions/cardGameActions";
+
+function FindKitchenItemsByWord() {
+  const { state, startGame, handleCardClick, toggleSound, dispatch } =
+    useCardGameContext();
+
+  useEffect(() => {
+    startGame(bathroomItems);
+    dispatch({ type: SET_DISPLAY_TEXT, payload: true });
+  }, []);
+
+  return (
+    <div className="find-alphabet-letters-game-background">
+      <ProgressComponent
+        progress={(state.currentItemIndex / bathroomItems.length) * 100}
+      />
+      <Container>
+        <Row>
+          <Col
+            className="card"
+            style={{ backgroundColor: "rgb(151, 212, 159)" }}
+          >
+            <p className="card__text">
+              {bathroomItems[state.currentItemIndex]}
+            </p>
+            <img
+              onClick={() => toggleSound("sound")}
+              className={`card__icon card__icon--sound ${
+                !state.soundON ? "card__icon--sound-disabled" : ""
+              }`}
+              src={sound}
+              alt="Toggle sound"
+            />
+            <img
+              className="card__icon card__icon--talking-sound"
+              src={talkingSound}
+              alt="Toggle voice sound"
+            />
+          </Col>
+        </Row>
+        <ImageCardComponents
+          chunkedArray={state.chunkedItems}
+          cardsBackgroundColors={state.itemsBackgroundColors}
+          handleCardClick={handleCardClick}
+        />
+      </Container>
+      <FeedbackMessageComponent isCorrect={state.isAnswerCorrect} />
+      {state.gameOver && <GameOverComponent />}
+    </div>
+  );
+}
+
+export default FindKitchenItemsByWord;
---
+++ src/pages/games/findImageByWord/FindBathroomItemsByWord/FindBathroomItemsByWordData.ts
@@ -0,0 +1,26 @@
+export const bathroomItems: string[] = [
+  "Dušo galvutė", // Showerhead
+  "Maišytuvas", // Faucet
+  "Vonia", // Bathtub
+  "Dušo užuolaida", // Shower curtain
+  "Rankšluostis", // Towel
+  "Vonios kilimėlis", // Bath mat
+  "Muilas", // Soap
+  "Muilo dozatorius", // Soap dispenser
+  "Šampūnas", // Shampoo
+  "Kondicionierius", // Conditioner
+  "Dušo želė", // Shower gel
+  "Dantų šepetėlis", // Toothbrush
+  "Dantų pasta", // Toothpaste
+  "Burnos skalavimo skystis", // Mouthwash
+  "Plaukų džiovintuvas", // Hairdryer
+  "Skustuvas", // Razor
+  "Tualetinis popierius", // Toilet paper
+  "Tualeto šepetys", // Toilet brush
+  "Unitazas", // Toilet
+  "Rankšluosčių laikiklis", // Towel rack
+  "Veidrodis", // Mirror
+  "Šukos", // Comb
+  "Kempinė", // Sponge
+  "Vonios spintelė", // Bathroom cabinet
+];
